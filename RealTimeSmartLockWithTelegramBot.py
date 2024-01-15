7cc346c41970098136aaca312309a113871c293d
EasyPeasyarduino <140818076+EasyPeasyarduino@users.noreply.github.com>
2024-01-15 13:25:10 +0200

    RealTimeSmartLockWithTelegramBot.py

--- RealTimeSmartLockWithTelegramBot.py
+++ RealTimeSmartLockWithTelegramBot.py
@@ -8,7 +8,7 @@ from telegram import InputFile
 
 # setting telegram
 API_TOKEN = 'Paste your API from TelegramBot'
-#get your telegram Id first 
+#get your telegram Id first from TelegramID.py
 OWNER_CHAT_ID='Paste Your ID'
 # Connect to Arduino via Serial
 arduino = serial.Serial('COM9', 9600)  # Update 'COM9' with your Arduino port
